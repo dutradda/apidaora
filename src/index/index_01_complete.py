4e6cc715c84b2aee3985e94f7f6a79498cb69273
Diogo Dutra <diogo.albuquerque@luizalabs.com>
2019-09-21 00:37:05 -0300

    Deployed 39ceb20 with MkDocs version: 1.0.4

--- src/index/index_01_complete.py
+++ src/index/index_01_complete.py
@@ -7,62 +7,50 @@ from apidaora import JSONResponse, MethodType, Request, Route, asgi_app
 
 
 @jsondaora
-class MyPathArgs(TypedDict):
-    name: str
-
-
-@jsondaora
-class MyQuery(TypedDict):
-    location: str
-
-
-@jsondaora
 class MyHeaders(TypedDict):
     x_req_id: str
 
 
 @jsondaora
-class MyBody(TypedDict):
-    last_name: str
-    age: int
-
-
-@jsondaora
 class MyRequest(Request):
+    class MyPathArgs(TypedDict):
+        name: str
+
+    class MyQuery(TypedDict):
+        location: str
+
+    class MyBody(TypedDict):
+        last_name: str
+        age: int
+
     path_args: MyPathArgs
     query: MyQuery
     headers: MyHeaders
     body: MyBody
 
 
-# if the class is not a TypedDict, jsondaora
-# will create a dataclass from it
-@jsondaora
-class You:
-    name: str
-    last_name: str
-    location: string(max_length=100)
-    age: integer(minimum=18)
-
-
-@jsondaora
-class MyResponseBody(TypedDict):
-    hello_message: str
-    about_you: You
-
-
 @jsondaora
 class MyResponse(JSONResponse):
+    class You:
+        name: str
+        last_name: str
+        location: string(max_length=100)
+        age: integer(minimum=18)
+
+    class MyResponseBody(TypedDict):
+        hello_message: str
+        about_you: 'MyResponse.You'
+
     body: MyResponseBody
     headers: MyHeaders
 
 
 def hello_controller(req: MyRequest) -> MyResponse:
-    body = MyResponseBody(
+    body = MyResponse.MyResponseBody(
         hello_message=hello_message(
             req.path_args['name'], req.query['location']
         ),
-        about_you=You(
+        about_you=MyResponse.You(
             name=req.path_args['name'],
             last_name=req.body['last_name'],
             location=req.query['location'],
