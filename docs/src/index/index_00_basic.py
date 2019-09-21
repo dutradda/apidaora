d5b5f3a518e6c239f00515d31a8e9783176d8941
Diogo Dutra <diogo.albuquerque@luizalabs.com>
2019-09-21 00:34:26 -0300

    Improve docs

--- docs/src/index/index_00_basic.py
+++ docs/src/index/index_00_basic.py
@@ -7,28 +7,24 @@ from apidaora import JSONResponse, MethodType, Request, Route, asgi_app
 
 
 @jsondaora
-class MyQuery(TypedDict):
-    name: str
-
-
-@jsondaora
 class MyRequest(Request):
+    class MyQuery(TypedDict):
+        name: str
+
     query: MyQuery
 
 
 @jsondaora
-class MyResponseBody(TypedDict):
-    message: str
-
-
-@jsondaora
 class MyResponse(JSONResponse):
+    class MyResponseBody(TypedDict):
+        message: str
+
     body: MyResponseBody
 
 
 def hello_controller(req: MyRequest) -> MyResponse:
     name = req.query['name']
-    body = MyResponseBody(message=f'Hello {name}!')
+    body = MyResponse.MyResponseBody(message=f'Hello {name}!')
     return MyResponse(HTTPStatus.OK, body=body)
 
 
