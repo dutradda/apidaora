8abf1108a5e813761fa9796cdd917c2139205645
Diogo Dutra <diogo.albuquerque@luizalabs.com>
2020-04-11 18:31:26 -0300

    Bump version: 0.17.2 → 0.18.0

--- apidaora/__init__.py
+++ apidaora/__init__.py
@@ -2,7 +2,7 @@
 ASGI App using dataclasses module for request/response objects
 """
 
-__version__ = '0.17.2'
+__version__ = '0.18.0'
 
 from apidaora.app import appdaora
 from apidaora.bodies import GZipFactory
