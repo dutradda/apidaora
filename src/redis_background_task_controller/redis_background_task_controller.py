3d72f303515a636339091b041b48b4fbbc9a7f6b
Diogo Dutra <diogo.albuquerque@luizalabs.com>
2020-04-13 00:36:37 -0300

    Deployed 54ba6fc with MkDocs version: 1.1

--- src/redis_background_task_controller/redis_background_task_controller.py
+++ src/redis_background_task_controller/redis_background_task_controller.py
@@ -7,7 +7,7 @@ from apidaora import appdaora, route
 REDIS_URI = f"redis://{os.environ.get('REDIS', '')}"
 
 
-@route.background('/hello', tasks_repository=REDIS_URI)
+@route.background('/hello', tasks_repository_uri=REDIS_URI)
 def hello_task(name: str) -> str:
     time.sleep(1)
     return f'Hello {name}!'
